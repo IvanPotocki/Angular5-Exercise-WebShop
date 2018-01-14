387cef37ba115c4b508a3c9b8fc723e270beb619
yeahnyea <ipotocki22@gmail.com>
2018-01-14 22:31:32 +0100

    Project Getting Started.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBRA1d0SH9_bcD-6RW4eeQxBvk0nQj0Wf0",
+    authDomain: "oshop-baf8f.firebaseapp.com",
+    databaseURL: "https://oshop-baf8f.firebaseio.com",
+    projectId: "oshop-baf8f",
+    storageBucket: "oshop-baf8f.appspot.com",
+    messagingSenderId: "173433335217"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBRA1d0SH9_bcD-6RW4eeQxBvk0nQj0Wf0",
+    authDomain: "oshop-baf8f.firebaseapp.com",
+    databaseURL: "https://oshop-baf8f.firebaseio.com",
+    projectId: "oshop-baf8f",
+    storageBucket: "oshop-baf8f.appspot.com",
+    messagingSenderId: "173433335217"
+  }
 };
